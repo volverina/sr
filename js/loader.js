ef1c0d46c28345be29bd880148012604aa9f871e
volverina <99404411+volverina@users.noreply.github.com>
2023-02-14 12:25:36 +0200

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,30 @@
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+
+import * as THREE from 'three';
+
+export const loadGLTF = (path) => {
+	return new Promise( (resolve, reject) => {
+		const loader = new GLTFLoader();
+		loader.load(path, (gltf) => {
+			resolve(gltf);
+		}),
+		(xhr) => {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded from '+ path );
+		},
+		(error) => {
+			reject(error);
+		}
+	});
+}
+
+
+export const loadAudio = (path) => {
+	return new Promise( (resolve, reject) => {
+		const loader = new THREE.AudioLoader();
+		loader.load(path, (gltf) => {
+			resolve(gltf);
+		});
+	});
+}
+
+
