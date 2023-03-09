628dd99b3d2f58b4425300879e401111f8c774e9
volverina <99404411+volverina@users.noreply.github.com>
2023-03-09 09:10:18 +0200

    Add files via upload

--- 0307/arcard.js
+++ 0307/arcard.js
@@ -108,6 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		//якір для WebGL
 		const anchor = mindarThree.addAnchor(0);
 
+		// додавання елементів до групи, пов'язаної із якорем 0 (фото візитівки)
 		anchor.group.add(card);
 		anchor.group.add(emailIcon);
 		anchor.group.add(locationIcon);
@@ -116,47 +117,154 @@ document.addEventListener("DOMContentLoaded", () => {
 		anchor.group.add(avatar.scene);
 		anchor.group.add(portfolioGroup);
 
+		// створення порожнього елементу HTML <div></div>
 		const textElement = document.createElement("div");
 		const textObj = new CSS3DObject(textElement);
 		textObj.position.set(0, -1000, 0);
-		//textObj.visible = false;
 		textElement.style.fontSize = "75px";
 		textElement.style.padding = "25px";
 		textElement.style.background = "#FFFFFF";
 
-		textElement.innerHTML = "Воно працює!";
+		textObj.visible = false;
+		//textElement.innerHTML = "Воно працює!";
 		
 		//якір для CSS
 		const cssAnchor = mindarThree.addCSSAnchor(0);
-
+		// додавання до якорю CSS елементу CSS div
 		cssAnchor.group.add(textObj);
 
+		// налаштування анімації моделі
 		const mixer = new THREE.AnimationMixer(avatar.scene);
 
 		if(avatar.animations.length != 0)
 		{
-			const action = mixer.clipAction(avatar.animations[1]);
+			const action = mixer.clipAction(avatar.animations[1]); // вибір анімації номер 1
 			action.play();
 		}
 
+		// створення таймер
 		const timer = new THREE.Clock();
+
+		// створення додаткового атрибуту clickable для об'єктів, що мають реагувати на натискання миші
+		emailIcon.userData.clickable = true;
+		locationIcon.userData.clickable = true;
+		webIcon.userData.clickable = true;
+		profileIcon.userData.clickable = true;
+		leftIcon.userData.clickable = true;
+		rightIcon.userData.clickable = true;
+		portfolioItem0.userData.clickable = true;
+		portfolioItem0V.userData.clickable = true;
+		portfolioItem1.userData.clickable = true;
+		portfolioItem2.userData.clickable = true;
+
+		// масив статитичних зображень елементів портфоліо
+		const portfolioItems = [
+			portfolioItem0,
+			portfolioItem1,
+			portfolioItem2
+		];
+		let currentPortfolio = 0; // поточний елемент для відображення
+
+		// універсальний обробник "дотику" до елементів сцени
+		document.body.addEventListener("click", (event) => {
+			const mouse_x = ( event.clientX / window.innerWidth ) * 2 - 1;
+			const mouse_y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+			const mouse = new THREE.Vector2(mouse_x, mouse_y);
+			const raycaster = new THREE.Raycaster();
+			raycaster.setFromCamera( mouse, camera );
+
+			const intersects = raycaster.intersectObjects(scene.children, true);
+
+			if(intersects.length != 0 )	//  є перетини
+			{
+				let o = intersects[0].object;
+
+				while(o.parent 	&& !o.userData.clickable)
+					o = o.parent;
+
+				if(o.userData.clickable)
+				{ 
+					if(o === leftIcon || o === rightIcon)
+					{
+						// 0, 1, 2
+						// right -> [0, 1, 2], [1, 2, 0], [2, 0, 1], [0, 1, 2]
+						// left -> [0, 1, 2], [2, 0, 1], [1, 2, 0], [0, 1, 2] 
+						if(o === leftIcon)
+							currentPortfolio = (currentPortfolio - 1 + portfolioItems.length) % portfolioItems.length;
+						if(o === rightIcon)
+							currentPortfolio = (currentPortfolio + 1) % portfolioItems.length;
+						portfolioItem0Video.pause();
+
+						for(let i =0; i < portfolioItems.length; i++)
+							portfolioGroup.remove(portfolioItems[i]);
+						portfolioGroup.add(portfolioItems[currentPortfolio]);
+					}
+					else if (o === portfolioItem0)
+					{
+						portfolioGroup.remove(portfolioItem0);
+						portfolioGroup.add(portfolioItem0V);
+						portfolioItems[0] = portfolioItem0V;
+						portfolioItem0Video.play();
+					}
+					else if (o === portfolioItem0V)
+					{
+						if(portfolioItem0Video.paused)
+							portfolioItem0Video.play();
+						else
+							portfolioItem0Video.pause();
+					}
+					else if (o === emailIcon)					
+					{
+						textObj.visible = true;
+						textElement.innerHTML = "semerikov@gmail.com";
+					}
+					else if (o === locationIcon)					
+					{
+						textObj.visible = true;
+						textElement.innerHTML = "Kryvyi Rih, Ukraine";
+					}
+					else if (o === webIcon)					
+					{
+						textObj.visible = true;
+						textElement.innerHTML = "https://kdpu.edu.ua/semerikov/";
+					}
+					else if (o === profileIcon)					
+					{
+						textObj.visible = true;
+						textElement.innerHTML = "https://kdpu.edu.ua/personal/cc.html";
+					}
+					else if (o === portfolioItem1)					
+					{
+						window.open("https://moodle.kdpu.edu.ua/course/view.php?id=8684");
+					}
+					else if (o === portfolioItem2)					
+					{
+						window.open("https://acnsci.org/journal/index.php/etq");
+					}
+
+				}
+			}
+		});
+
 
 		//4. Запуск MindAR 
 		await mindarThree.start();
 
 		//5. Цикл оновлення
 		renderer.setAnimationLoop( () => {
+			// delta - час, що минув від попереднього оновлення сцени; elapsed - загальний час
 			const delta = timer.getDelta();
 			const elapsed = timer.getElapsedTime();
-			mixer.update(delta);
+			mixer.update(delta); //відтворити фрагмент анімації за час delta
 
-			const iconScale = 1 + 0.25 * Math.sin(5 * elapsed);
+			const iconScale = 1 + 0.25 * Math.sin(5 * elapsed); // масштаб: [0.75; 1.25]
 
+			// анімація іконок шляхом зміни їх масштабу
 			[emailIcon, locationIcon, webIcon, profileIcon].forEach( (icon) => {
 				icon.scale.set(iconScale, iconScale, iconScale);
 			});
 
-
+			// рух моделі залежно від часу по вісі Z
 			avatar.scene.position.set(-0.4, -0.4, -0.5 + Math.sin(1 * elapsed));
 			
 			renderer.render(scene, camera);
