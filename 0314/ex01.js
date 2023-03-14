06b9db3c44ca688509b8183016444a11f2af3962
volverina <99404411+volverina@users.noreply.github.com>
2023-03-14 12:19:09 +0200

    Add files via upload

---
+++ 0314/ex01.js
@@ -0,0 +1,100 @@
+import * as THREE from '../js/three/three.module.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+	//основна функція
+	const initialize = async() => {
+		// посилання на кнопку
+		const arButton = document.querySelector("#ar-button");
+
+		// перевірка доступності на пристрої WebXR
+		const supported = navigator.xr 
+			&&
+			await navigator.xr.isSessionSupported("immersive-ar");
+
+		if(!supported)  // відключаємо кнопку за відсутності підтримки
+		{
+			arButton.textContent = "На жаль, WebXR не підтримується";
+			arButton.disabled = true;
+			return;
+		}
+
+		// створення сцени з червоним кубом розміром 5 см
+
+	        let scene = new THREE.Scene();
+	        let camera = new THREE.PerspectiveCamera();
+
+		let renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true
+		});
+	        renderer.setSize(window.innerWidth, window.innerHeight);
+	        renderer.setPixelRatio(window.devicePixelRatio);
+
+	        document.body.appendChild(renderer.domElement);
+
+		const cubegeometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+		const cubematerial = new THREE.MeshBasicMaterial({color:'red'})
+
+	        let cube = new THREE.Mesh(cubegeometry, cubematerial);
+
+	        cube.position.set(0, 0, -0.3);
+		scene.add(cube);
+       
+        	var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        	scene.add(light);
+
+		// перевірка запуску та завершення сесії WebXR
+		renderer.xr.addEventListener("sessionstart", (evt) => {
+			console.log("Сесію WebXR розпочато");
+		});
+
+		renderer.xr.addEventListener("sessionend", (evt) => {
+			console.log("Сесію WebXR завершено");
+		});
+
+		let currentSession = null; // ідентифікатор сеансу 
+
+		//console.log("We are here");
+
+		// визначення функції start
+		const start = async() => {
+			// запит сеансу доповненої реальності	
+			currentSession = await navigator.xr.requestSession("immersive-ar", {
+				optionalFeatures: ["dom-overlay"],
+				domOverlay: {root: document.body},
+			});
+
+			console.log(currentSession);
+			// повідомлення рушія Three.js про параметри використання WebXR
+			renderer.xr.enabled = true;
+			renderer.xr.setReferenceSpace("local");
+			await renderer.xr.setSession(currentSession);
+
+			console.log(renderer.xr);
+			renderer.setAnimationLoop(() => {
+			    renderer.render(scene, camera);
+			}); 
+		
+			arButton.textContent = "Завершити сесію WebXR";
+
+		}
+
+		// визначення функції end
+		const end = async() => {
+			currentSession.end(); // завершення сеансу
+			renderer.setAnimationLoop(null);
+			renderer.clear();
+			arButton.style.display="none";
+		}
+
+		// натискання кнопки
+		arButton.addEventListener("click", () => {
+			if(currentSession) // завершити сеанс
+				end();
+			else // почати сеанс
+				start();
+		});
+	}
+
+	initialize(); // розпочати роботу
+});
